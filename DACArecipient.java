e01d0b06925356752ac0797204891204be618604
Ricardo Rodriguez <lucario.the.hero@gmail.com>
2026-05-07 05:04:04 +0000

    First push 2

--- DACArecipient.java
+++ DACArecipient.java
@@ -21,13 +21,89 @@ public class DACArecipient
 {
 	/***** INSTANCE VARIABLES *****/
 	//TODO: Declare the instance variables. 
+	private String surname;
+	private String givenName;
+	private String uscisNumber;
+	private String countryOfOrigin;
+	private String birthday;
+	private String validFromDate;
+	private String expirationDate;
+	private String sex;
 
+
+public String getSurname()
+	{
+		return this.surname;
 	/***** ACCESSORS *****/
 	//TODO: Write the getter for each instance variable. Remember to include documentation for each method.
-	
-	/***** MUTATORS *****/
-	//TODO: Write the setter for each instance variable. Remember to include documentation for each method.
+		
+}
 
+public void setSurname (String surname){
+		this.surname = surname;
+	/***** MUTATORS *****/
+
+	//TODO: Write the setter for each instance variable. Remember to include documentation for each method.
+	}
+
+	public String getGivenName(){
+	
+	    return this.givenName;
+	
+	}
+
+	public String getUscisNumber(){
+	
+	    return this.uscisNumber;
+	
+	}
+
+	public String getCountryOfOrigin(){
+	
+	    return this.countryOfOrigin;
+	
+	}
+	public String getBirthday(){
+	
+	    return this.birthday;
+	
+	}
+	public String getValidFromDate(){
+	
+	    return this.validFromDate;
+	
+	}
+	public String getExpirationDate(){	
+			
+	    return this.expirationDate;
+	
+	}	
+	public String getSex(){
+	
+	    return this.sex;
+	}
+
+public void setAll(String surname, String givenName, String uscisNumber, String countryOfOrigin, String birthday, String validFromDate, String expirationDate, String sex) {
+	
+	this.surname = surname; // or setSurname(surname);
+	this.givenName = givenName;
+	this.uscisNumber = uscisNumber;
+	this.countryOfOrigin = countryOfOrigin;
+	this.birthday = birthday;
+	this.validFromDate = validFromDate;
+	this.expirationDate = expirationDate;
+	this.sex = sex;
 	//TODO: Write the setAll method. Remember to include documentation.
+	surname = "Chapeton-Lamas";
+	givenName = "Nery";
+	uscisNumber = "12-4-789";
+	countryOfOrigin = "Guatemala";
+	birthday = "2451564";
+	validFromDate = "3956753";
+	expirationDate = "3956840";
+	sex = "M";
+	}
+
+	
 
 }
